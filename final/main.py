26fc3177d0e0cee490002b35980509d50097484a
Willy Tai <b04502139@ntu.edu.tw>
2019-06-18 12:23:57 +0800

    [content warp] remove create_feat.py

--- final/main.py
+++ final/main.py
@@ -1,9 +1,23 @@
 from contentwarp import ContentWarp
+from argparse import ArgumentParser
 import sys
+import os
 
 
 def main():
-    warp = ContentWarp(sys.argv[1], '0007.txt', 18, 32, alpha=0.5)
+    parser = ArgumentParser()
+    parser.add_argument('--image_dir', required=True)
+    parser.add_argument('--feature_dir', required=True)
+    args = parser.parse_args()
+
+    images   = os.listdir(args.image_dir)
+    features = os.listdir(args.feature_dir)
+
+    print (images)
+    print (features)
+
+    sys.exit()
+    warp = ContentWarp(sys.argv[1], sys.argv[2], 18, 32, alpha=0.5)
 
 
 if __name__ == '__main__':
